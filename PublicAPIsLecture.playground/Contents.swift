028dff836c43a9eaf03b4eab9b470d877b51f8cd
Ian Becker <ian-becker@lambdastudents.com>
2020-08-03 10:25:27 -0700

    Model Complete

--- PublicAPIsLecture.playground/Contents.swift
+++ PublicAPIsLecture.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var str = "Hello, playground"
+// MARK: - Goals
+// GET all categories - fetch
+// GET all API's for a specific category - fetch
+
+let baseURL = URL(string: "https://api.publicapis.org")
+
+// MARK: - Model
+struct TopLevelObject {
+    let count: Int
+    let entries: [Entry]
+}
+
+struct Entry {
+    let API: String
+    let Description: String
+    let Auth: String
+    let HTTPS: Bool
+    let Cors: String
+    let Link: String
+    let Category: String
+}
